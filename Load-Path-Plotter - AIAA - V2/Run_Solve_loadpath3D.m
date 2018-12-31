4d86ec6b303b61b4f6b4033ba149b328744f31d4
KhanMechAI <khan.schroderturner@gmail.com>
2018-12-30 16:08:03 -0800

    added tic and toc to time the computation

--- Load-Path-Plotter - AIAA - V2/Run_Solve_loadpath3D.m
+++ Load-Path-Plotter - AIAA - V2/Run_Solve_loadpath3D.m
@@ -2,7 +2,7 @@ function Run_Solve_loadpath3D(sim_dir, seed_dir, save_dir, model_name,path_dir,.
                     pulse, parallel, newPDF, recompute, step_size, path_length,...
                     plot_minimum_vector, plot_maximum_vector)
 %% ********************  House Keeping   ******************************
-
+tic
 %TODO: Parallel computing curently buggy due to recent changes. Need to correct the function inputs and include the transient code.
 % Closes previously opened waitbars
     F = findall(0,'type','figure','tag','TMWWaitbar');
@@ -365,7 +365,7 @@ function Run_Solve_loadpath3D(sim_dir, seed_dir, save_dir, model_name,path_dir,.
                     z = [];
                     intense = [];
                     kdk = 1;
-                    kkdk = 0;                      
+                    kkdk = 0;
                     [mdk,ndk] = size(dkintense);
                     while kdk < ndk;
                         %Only plot path if magnitude of pointing
@@ -456,6 +456,7 @@ function Run_Solve_loadpath3D(sim_dir, seed_dir, save_dir, model_name,path_dir,.
     fprintf('Load paths complete.\n')
 
     %%********************************End of Computation******************************
+    toc
 end
 function [] = modelPlot3D(x_paths,y_paths,z_paths,Intensity,PartArr,nodes,pulse)
     %Just some custom settings for plotting the paths
